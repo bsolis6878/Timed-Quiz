5a4813f2f0ee3ef7fdcdd4fb8b6ad2e286af24aa
bsolis6878 <bsolis6878@gmail.com>
2022-08-06 18:10:15 -0500

    moves on to next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,26 +55,26 @@ var startGame = function() {
 var startQuiz = function() {
     var question = document.createElement("h1");
     question.className = "questions";
-    question.textContent = questions[0].question;
+    question.innerHTML = questions[0].question + "<br />";
     questionBox.appendChild(question);
 
     var answerOne = document.createElement("button")
-    answerOne.className = "wrong-answers";
+    answerOne.className = "answers";
     answerOne.textContent = questions[0].answerOne;
     question.appendChild(answerOne);
 
     var answerTwo = document.createElement("button")
-    answerTwo.className = "wrong-answers";
+    answerTwo.className = "answers";
     answerTwo.textContent = questions[0].answerTwo;
     question.appendChild(answerTwo);
 
     var answerThree = document.createElement("button")
-    answerThree.className = "wrong-answers";
+    answerThree.className = "answers";
     answerThree.textContent = questions[0].answerThree;
     question.appendChild(answerThree);
 
     var answerFour = document.createElement("button")
-    answerFour.className = "correct-answers";
+    answerFour.className = "answers";
     answerFour.textContent = questions[0].answerFour;
     question.appendChild(answerFour);
 
@@ -82,12 +82,45 @@ var startQuiz = function() {
         alert("Correct, plus 10 points!");
         score += 10;
         console.log(score);
+        question.remove();
+        answerOne.remove();
+        answerTwo.remove();
+        answerThree.remove();
+        answerFour.remove();
+        nextQuestion();
     }
-    
+
     answerOne.addEventListener("click", wrong);
     answerTwo.addEventListener("click", wrong);
     answerThree.addEventListener("click", wrong);
     answerFour.addEventListener("click", right);
+}
+
+var nextQuestion = function() {
+    var question = document.createElement("h1");
+    question.className = "questions";
+    question.innerHTML = questions[1].question + "<br />";
+    questionBox.appendChild(question);
+
+    var answerOne = document.createElement("button")
+    answerOne.className = "answers";
+    answerOne.textContent = questions[1].answerOne;
+    question.appendChild(answerOne);
+
+    var answerTwo = document.createElement("button")
+    answerTwo.className = "answers";
+    answerTwo.textContent = questions[1].answerTwo;
+    question.appendChild(answerTwo);
+
+    var answerThree = document.createElement("button")
+    answerThree.className = "answers";
+    answerThree.textContent = questions[1].answerThree;
+    question.appendChild(answerThree);
+
+    var answerFour = document.createElement("button")
+    answerFour.className = "answers";
+    answerFour.textContent = questions[1].answerFour;
+    question.appendChild(answerFour);
 }
 
 button.addEventListener("click", startGame);
