f35c127da8f140b90861d17a3732c964f0dbfb2b
bsolis6878 <bsolis6878@gmail.com>
2022-08-06 14:16:50 -0500

    Added timer functionality, working on creating questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,35 @@
 var button = document.querySelector("#button");
 var startingText = document.querySelector(".starting-text");
 var welcomeText = document.querySelector(".welcome-text");
+var questionBox = document.querySelector(".starting");
+
+var score = 0;
+var questions
+
 
 var startGame = function() {
     startingText.remove();
     button.remove();
-    welcomeText.remove();
+
+    startQuiz();
+
+    var timeLeft = 5;
+    var timer = setInterval(function() {
+        if (timeLeft > 1) {
+            welcomeText.textContent = timeLeft + " seconds remaining!";
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            welcomeText.textContent = timeLeft + " second remaining!";
+            timeLeft--;
+        } else {
+            clearInterval(timer);
+            welcomeText.textContent = "Time's up!"
+        }
+    }, 1000);
+}
+
+var startQuiz = function() {
+
 }
 
 button.addEventListener("click", startGame);
