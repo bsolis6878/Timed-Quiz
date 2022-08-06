6bbe6b2da349a97d38f63e06ea03673613831a15
bsolis6878 <bsolis6878@gmail.com>
2022-08-06 16:06:59 -0500

    got first question to display properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,30 @@ var welcomeText = document.querySelector(".welcome-text");
 var questionBox = document.querySelector(".starting");
 
 var score = 0;
-var questions
+var questions = [
+    {
+        question: "Which country has the highest life expectancy?",
+        answerOne: "United States",
+        answerTwo: "Sweden",
+        answerThree: "Zimbabwe",
+        answerFour: "Hong Kong"
+    },
+    {
+        question: "What is the most common surname in the United States?",
+        answerOne: "Johnson",
+        answerTwo: "Smith",
+        answerThree: "Jones",
+        answerFour: "Williams"
+    }
+]
 
 
 var startGame = function() {
     startingText.remove();
     button.remove();
+    welcomeText.textContent = "60 seconds remaining!";
 
-    startQuiz();
-
-    var timeLeft = 5;
+    var timeLeft = 60;
     var timer = setInterval(function() {
         if (timeLeft > 1) {
             welcomeText.textContent = timeLeft + " seconds remaining!";
@@ -26,10 +40,35 @@ var startGame = function() {
             welcomeText.textContent = "Time's up!"
         }
     }, 1000);
+
+    startQuiz();
 }
 
 var startQuiz = function() {
+    var question = document.createElement("h1");
+    question.className = "questions";
+    question.textContent = questions[0].question;
+    questionBox.appendChild(question);
 
+    var answerOne = document.createElement("button")
+    answerOne.className = "answers";
+    answerOne.textContent = questions[0].answerOne;
+    question.appendChild(answerOne);
+
+    var answerTwo = document.createElement("button")
+    answerTwo.className = "answers";
+    answerTwo.textContent = questions[0].answerTwo;
+    question.appendChild(answerTwo);
+
+    var answerThree = document.createElement("button")
+    answerThree.className = "answers";
+    answerThree.textContent = questions[0].answerThree;
+    question.appendChild(answerThree);
+
+    var answerFour = document.createElement("button")
+    answerFour.className = "answers";
+    answerFour.textContent = questions[0].answerFour;
+    question.appendChild(answerFour);
 }
 
 button.addEventListener("click", startGame);
